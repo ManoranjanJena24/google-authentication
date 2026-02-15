7598b56cdef06720e425f5aa6c658ca31ffcf8b4
Manoranjan Jena <manoranjanjenamitsy17@gmail.com>
2026-02-15 18:26:52 +0530

    Done with the code getting from google

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,23 +3,29 @@ import {BrowserRouter , Route ,Routes ,Navigate } from 'react-router-dom'
 import GoogleLogin from './GoogleLogin'
 import Dashboard from './Dashboard'
 import PageNotFound from './PageNotFound'
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
+
+  const GoogleAuthWrapper = ()=>{
+    return (
+      <GoogleOAuthProvider clientId="981534546844-q8mnlkdu1lneukqncplc7titeq6ijr7j.apps.googleusercontent.com">
+        <GoogleLogin></GoogleLogin>
+      </GoogleOAuthProvider>
+    );
+  }
  
 
   return (
     <BrowserRouter>
-    <Routes>
-
-      <Route path="/login" element={<GoogleLogin/>} />
-      <Route path="/" element={<Navigate to="/login"/>} />
-      <Route path="/dashboard" element={<Dashboard/>} />
-      <Route path="*" element={<PageNotFound/>} />
-
-    </Routes>
-   
+      <Routes>
+        <Route path="/login" element={<GoogleAuthWrapper />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
--- Frontend/src/GoogleLogin.jsx
+++ Frontend/src/GoogleLogin.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useGoogleLogin } from "@react-oauth/google";
 
 const GoogleLogin = () => {
-  return (
-    <div>GoogleLogin</div>
-  )
-}
+  const responseGoogle = async (authResult) => {
+    try {
+      console.log(authResult);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-export default GoogleLogin
+  const googleLogin = useGoogleLogin({
+    onSuccess: responseGoogle,
+    onError: (error) => console.log(error),
+    flow: "auth-code",
+  });
+
+  return (
+    <div className="App">
+      <button onClick={() => googleLogin()}>Login With Google</button>
+    </div>
+  );
+};
+
+export default GoogleLogin;
